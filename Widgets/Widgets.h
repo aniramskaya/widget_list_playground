1ec01e1f2ff621bc8173306d90c918d3afb0a172
Marina Chemezova <aniramskaya@gmail.com>
2023-06-07 14:11:37 +0500

    Initial Commit

---
+++ Widgets/Widgets.h
@@ -0,0 +1,18 @@
+//
+//  Widgets.h
+//  Widgets
+//
+//  Created by Марина Чемезова on 07.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Widgets.
+FOUNDATION_EXPORT double WidgetsVersionNumber;
+
+//! Project version string for Widgets.
+FOUNDATION_EXPORT const unsigned char WidgetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Widgets/PublicHeader.h>
+
+

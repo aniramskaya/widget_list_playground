86fcaeac4addebe441b7a8f901e96f7ee816f248
Marina Chemezova <aniramskaya@gmail.com>
2023-06-08 18:29:53 +0500

    WidgetListLoader коммент про назначение класса

--- Modules/CDEKUI/Widget/WidgetListLoader.swift
+++ Modules/CDEKUI/Widget/WidgetListLoader.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Инстанцирует и конфигурирует виджеты с помощью items: [UIWidgetLoader] и реализует логику загрузки обязательных/необязательных виджетов с таймаутом, как было в шоппинге
 class WidgetListLoader {
     func load(items: [UIWidgetLoader], timeout: TimeInterval, completion: @escaping (Result<[any UIWidget], Swift.Error>) -> Void) {
         // TODO: Loading logic
